e4a9d102a002706e6077587f45eae2b21bdd7760
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-23 15:10:11 +0900

    [update] OneToOne Mapping

--- src/main/java/com/jpa/exercise/domain/Book.java
+++ src/main/java/com/jpa/exercise/domain/Book.java
@@ -18,4 +18,8 @@ public class Book {
     @ManyToOne
     @JoinColumn(name = "author_id")
     private Author author;
+
+    @OneToOne
+    @JoinColumn(name = "publisher_id")
+    private Publisher publisher;
 }
