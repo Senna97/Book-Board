2b60235fce1e32a9ed9b48fdd052b14fe434e6eb
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-23 15:09:20 +0900

    [add] Publisher

---
+++ src/main/java/com/jpa/exercise/domain/Publisher.java
@@ -0,0 +1,21 @@
+package com.jpa.exercise.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Publisher {
+    @Id
+    private Long id;
+    private String name;
+    private String address;
+}
