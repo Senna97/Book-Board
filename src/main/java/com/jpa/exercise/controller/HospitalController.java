46458439a47946051b07e008ba845181b8206307
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-25 14:10:33 +0900

    해당 병원의 리뷰를 모두 조회하는 기능 추가

--- src/main/java/com/jpa/exercise/controller/HospitalController.java
+++ src/main/java/com/jpa/exercise/controller/HospitalController.java
@@ -2,13 +2,13 @@ package com.jpa.exercise.controller;
 
 import com.jpa.exercise.domain.dto.ReviewCreateRequest;
 import com.jpa.exercise.domain.dto.ReviewCreateResponse;
+import com.jpa.exercise.domain.dto.ReviewReadResponse;
 import com.jpa.exercise.service.ReviewService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/hospitals")
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HospitalController {
 
     private final ReviewService reviewService;
-
 
     public HospitalController(ReviewService reviewService) {
         this.reviewService = reviewService;
@@ -28,5 +27,10 @@ public class HospitalController {
         return ResponseEntity.ok().body(reviewService.createReview(reviewCreateRequest));
         // 에러 처리를 아직 하지 않았기 때문에 ok()로 한다.
         // 컨트롤러는 사용자의 요청을 받아서 받은 요청을 서비스로 보내고 서비스에서 리턴한 것을 다시 사용자에게 응답한다.
+    }
+
+    @GetMapping("/{hospitalId}/reviews")
+    public ResponseEntity<List<ReviewReadResponse>> viewAllReviews(@PathVariable Long hospitalId) {
+        return ResponseEntity.ok().body(reviewService.viewReviewList(hospitalId));
     }
 }
--- src/main/java/com/jpa/exercise/service/ReviewService.java
+++ src/main/java/com/jpa/exercise/service/ReviewService.java
@@ -4,11 +4,14 @@ import com.jpa.exercise.domain.Hospital;
 import com.jpa.exercise.domain.Review;
 import com.jpa.exercise.domain.dto.ReviewCreateRequest;
 import com.jpa.exercise.domain.dto.ReviewCreateResponse;
+import com.jpa.exercise.domain.dto.ReviewReadResponse;
 import com.jpa.exercise.repository.HospitalRepository;
 import com.jpa.exercise.repository.ReviewRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ReviewService {
@@ -49,5 +52,23 @@ public class ReviewService {
         Review review = reviewRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("입력하신 id에 해당하는 리뷰가 없습니다."));
         return review;
+    }
+
+    public List<ReviewReadResponse> viewReviewList(Long hospitalId) {
+        Hospital hospital = hospitalRepository.findById(hospitalId)
+                .orElseThrow(() -> new IllegalArgumentException("입력하신 id에 해당하는 병원이 없습니다."));
+
+        List<ReviewReadResponse> reviewList = reviewRepository.findByHospital(hospital)
+                .stream()
+                .map(review -> ReviewReadResponse.builder()
+                        .id(review.getId())
+                        .title(review.getTitle())
+                        .content(review.getContent())
+                        .userName(review.getUserName())
+                        .hospitalName(review.getHospital().getHospitalName())
+                        .build())
+                .collect(Collectors.toList());
+
+        return reviewList;
     }
 }
