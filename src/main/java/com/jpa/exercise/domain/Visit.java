b0c508cb0f59d090bd6fc09214571602e2426dcb
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-12-07 15:07:09 +0900

    [add]Visit Entity

---
+++ src/main/java/com/jpa/exercise/domain/Visit.java
@@ -0,0 +1,21 @@
+package com.jpa.exercise.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Visit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String disease;
+    private float amount;
+}
